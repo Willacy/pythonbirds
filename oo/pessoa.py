91ad5324fbc333befca686d2b65d0295f1cb63a8
willacy <wannicy@gmail.com>
2020-08-14 15:59:54 -0300

    Criado atributos complexo filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,8 +1,21 @@
 # Criando a Classe Pessoa
 class Pessoa:
+    def __init__(self, *filhos,nome=None, idade=35):
+        self.idade = idade
+        self.nome = nome
+        self.filhos = list(filhos)
+
     def cumprimentar(self):
-        return 'Olá'
+        return f'{id(self)}'
+
 
 if __name__ == '__main__':
-    p = Pessoa()
-    print(p.cumprimentar())
+    renzo = Pessoa(nome='Renzo')
+    willacy = Pessoa(renzo, nome='Willacy')
+    print(Pessoa.cumprimentar(willacy))
+    print(id(willacy))
+    print(willacy.cumprimentar())
+    print(willacy.nome)
+    print(willacy.idade)
+    for filho in willacy.filhos:
+        print(filho.nome)
